39ddda2cfe3de9a24fe64ac7b130247277be84f8
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2021-08-15 19:21:17 +0900

    背景に四角をランダム表示する

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import * as THREE from 'three';
 
-window.addEventListener('load', init);
+(() =>  {
+    const canvas = document.getElementById('canvas')
+    const scene = new THREE.Scene()
 
-let scene
+    const renderer =  new THREE.WebGLRenderer()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
+    canvas.appendChild(renderer.domElement)
 
-//Renderer
-const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#myCanvas')
-});
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
+    const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth/window.innerHeight,
+        0.1,
+        100
+    )
 
-function init(){
-    scene = new THREE.Scene();
-    console.log(scene);
-}
+    camera.position.z = 5
+    scene.add(camera)
+
+    const geometry = new THREE.BufferGeometry()
+    const count = 1000
+    const position = new Float32Array(count * 3)
+    for(let i = 0; i < count * 3; i++) {
+        position[i] = (Math.random() - 0.5) * 10
+    }
+
+    geometry.setAttribute('position', new THREE.BufferAttribute(position, 3))
+
+    const material = new THREE.PointsMaterial({
+        size: 0.3,
+        sizeAttenuation: true,
+        color: new THREE.Color('#FFFFFF')
+    })
+
+    const particle = new THREE.Points(geometry, material)
+    scene.add(particle)
+
+    const raf = () => {
+        renderer.render(scene, camera)
+        window.requestAnimationFrame(raf)
+    }
+
+    raf()
+})();
